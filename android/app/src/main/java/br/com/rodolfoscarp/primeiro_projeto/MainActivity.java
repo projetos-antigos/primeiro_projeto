3af1fc15f5ffbd79079ecca2320287dfe4593364
rodolfoscarp <rodolfoscarp@gmail.com>
2019-09-13 13:36:41 -0300

    inicialização

---
+++ android/app/src/main/java/br/com/rodolfoscarp/primeiro_projeto/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.rodolfoscarp.primeiro_projeto;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
